26549274f3ad57b4b56c55482326559fd50c001e
Martinsbl <martinsbl@gmail.com>
2018-06-24 18:13:56 +0200

    ble updates

--- ble_servo.c
+++ ble_servo.c
@@ -3,13 +3,58 @@
 #include "ble_srv_common.h"
 #include "ble_conn_state.h"
 #include "nrf_log.h"
+#include "app_error.h"
 #include "ble_servo.h"
 
+
+void on_write_event(ble_evt_t const * p_ble_evt, ble_servo_t * p_ble_servo)
+{
+    NRF_LOG_INFO("Received data!");
+    
+    // Get the Write event data structure
+    ble_gatts_evt_write_t const * p_evt_write = &p_ble_evt->evt.gatts_evt.params.write;
+    
+    // Get length of data and pointer to the actual data
+    uint16_t data_length = p_evt_write->len;
+    uint8_t const * data = p_evt_write->data;
+    
+    // Check whether the data written was a servo value. 
+    if(p_evt_write->handle == p_ble_servo->servo_value_handles.value_handle)
+    {
+	uint16_t new_servo_value = 0;
+	
+	// Convert data to an uint16_t. 
+	if(data_length == 1)
+	{
+	    new_servo_value = data[0];	
+	}
+	else if(data_length == 2)
+	{
+	    new_servo_value= (data[0] << 8) | data[1];
+	}
+	else
+	{
+	    // Should never happen.
+	    new_servo_value = 0; 
+	}
+
+	NRF_LOG_INFO("Got new servo values:");
+	NRF_LOG_INFO("Value: 0x%04X (%d)", new_servo_value, new_servo_value);
+	NRF_LOG_INFO("Data lenght: %d", data_length);
+	
+	// Call the Servo Service event handler. This should call set_servo_value() in main.c
+	p_ble_servo->evt_handler(new_servo_value);
+    }
+}
+
+/** Function used to propagate BLE events from main.c into ble_servo.c
+ */
 void ble_servo_on_ble_evt(ble_evt_t const * p_ble_evt, ble_servo_t * p_ble_servo)
 {
     switch (p_ble_evt->header.evt_id)
     {
         case BLE_GATTS_EVT_WRITE:
+	    on_write_event(p_ble_evt, p_ble_servo);
             break;
 
         default:
@@ -19,23 +64,70 @@ void ble_servo_on_ble_evt(ble_evt_t const * p_ble_evt, ble_servo_t * p_ble_servo
 }
 
 
-static ret_code_t servo_char_add(ble_servo_t * p_ble_servo)
+/** Function for initializing our Servo Characteristic. 
+ */
+static void servo_char_add(ble_servo_t * p_ble_servo)
 {
     ret_code_t          err_code;
     
     ble_gatts_char_md_t char_md;
     ble_gatts_attr_t    attr_char_value;
-    ble_uuid_t          ble_uuid;
+    ble_uuid_t          characteristic_uuid;
     ble_gatts_attr_md_t attr_md;
+    
+    //TODO Initialize the characteristic
+    memset(&char_md, 0, sizeof(char_md));
 
-    return NRF_SUCCESS;
+    char_md.char_props.write         = 1;
+
+    characteristic_uuid.type = p_ble_servo->uuid_index;
+    characteristic_uuid.uuid = SERVO_CHARACTERISTIC_UUID;
+
+    memset(&attr_md, 0, sizeof(attr_md));
+
+    BLE_GAP_CONN_SEC_MODE_SET_NO_ACCESS(&attr_md.read_perm); // Set security mode to require no protection, open link
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&attr_md.write_perm); // Set security mode to require no protection, open link
+
+    attr_md.vloc    = BLE_GATTS_VLOC_STACK; // We chose to store the characteristic value in Softdevice (stack) controlled part of memory.
+
+    memset(&attr_char_value, 0, sizeof(attr_char_value));
+
+    attr_char_value.p_uuid    = &characteristic_uuid;
+    attr_char_value.p_attr_md = &attr_md;
+    attr_char_value.init_len  = sizeof(uint16_t);
+    attr_char_value.max_len   = sizeof(uint16_t);
+
+    err_code = sd_ble_gatts_characteristic_add(p_ble_servo->service_handle,
+                                           &char_md,
+                                           &attr_char_value,
+                                           &p_ble_servo->servo_value_handles);
+    APP_ERROR_CHECK(err_code); 
 }
 
 
-ret_code_t ble_servo_init(ble_servo_t * p_ble_servo)
+/** Function for initializing our Servo service. 
+ */
+ret_code_t ble_servo_service_init(ble_servo_t * p_ble_servo)
 {
     ret_code_t err_code;
-    ble_uuid_t ble_uuid;
+    ble_uuid_t service_uuid;
+    ble_uuid128_t servo_base_uuid = SERVO_BASE_UUID;
+    
+
+    // Adding proprietary Service to the SoftDevice
+    err_code = sd_ble_uuid_vs_add(&servo_base_uuid, &p_ble_servo->uuid_index); // Add SERVO_BASE_UUID to table of UUIDs and get the UUID's index
+    APP_ERROR_CHECK(err_code); 
+    
+    service_uuid.type = p_ble_servo->uuid_index;
+    service_uuid.uuid = SERVO_SERVICE_UUID; 
+    
+    // Add the service.
+    err_code = sd_ble_gatts_service_add(BLE_GATTS_SRVC_TYPE_PRIMARY,
+                                        &service_uuid,
+                                        &p_ble_servo->service_handle);
+					
+					
+    servo_char_add(p_ble_servo);
 
     return NRF_SUCCESS;
 }
--- ble_servo.h
+++ ble_servo.h
@@ -8,35 +8,39 @@
 #include "ble_srv_common.h"
 #include "nrf_sdh_ble.h"
 
-/**@brief Macro for defining a ble_servo instance.
- *
- * @param   _name  Name of the instance.
- * @hideinitializer
+/* The base UUID (also known as vendor specific UUID) for our Service and Characteristic 
+ * Properly formatted it looks like this: 
+ * F364xxxx-01B0-4240-BA50-05CA55BF8ABC
  */
-#define BLE_SERVO_DEF(_name)                          \
-    static ble_servo_t _name;                         \
-    NRF_SDH_BLE_OBSERVER(_name ## _obs,             \
-                         BLE_SERVO_BLE_OBSERVER_PRIO, \
-                         ble_servo_on_ble_evt,        \
-                         &_name)
+#define SERVO_BASE_UUID		   {0xBC, 0x8A, 0xBF, 0x55, 0xCA, 0x05, 0x50, 0xBA, \
+				    0x40, 0x42, 0xB0, 0x01, 0x00, 0x00, 0x64, 0xF3}
+
+#define SERVO_SERVICE_UUID	    0x5E55 // 16 bit UUID for our Service
+#define SERVO_CHARACTERISTIC_UUID   0x5E5C // 16 bit UUID for our Characteristic
 
 
-/**@brief Servo Service event handler type. */
-typedef void (* ble_servo_evt_handler_t) (uint16_t pwm_value);
+
+/** Servo Service event handler. 
+ * This is just a simple event handler used to pass new servo values from ble_servo.c to main.c
+ */
+typedef void (* ble_servo_evt_handler_t) (uint16_t servo_value);
 
 
 /**@brief Servo Service structure. This contains various status information for the service. */
 typedef struct
 {
-    ble_servo_evt_handler_t    evt_handler;               /**< Event handler to be called for handling events in the Servo Service. */
+    ble_servo_evt_handler_t  evt_handler;               /**< Event handler to be called for handling events in the Servo Service. */
     uint16_t                 service_handle;            /**< Handle of Servo Service (as provided by the BLE stack). */
-    ble_gatts_char_handles_t servo_value_handles;     /**< Handles related to the Servo Level characteristic. */
+    ble_gatts_char_handles_t servo_value_handles;	/**< Handles related to the Servo Level characteristic. */
+    uint8_t                  uuid_index;		/**< The softdevice will make a table of UUIDs. This variable is used to refer to an index in that table to get the base UUID. */
 }ble_servo_t;
 
-
+/** Function for initializing our Servo service. 
+ */
 ret_code_t ble_servo_service_init(ble_servo_t * p_servo);
 
-
+/** Function used to propagate BLE events from main.c into ble_servo.c
+ */
 void ble_servo_on_ble_evt(ble_evt_t const * p_ble_evt, ble_servo_t * p_ble_servo);
 
 #endif // BLE_SERVO_H__
